4c4bb5656c8ad5d8b25b7f9a2c3799818025b28f
Niraj Paudel <77-bca.12@texasintl.edu.np>
2024-05-24 18:26:02 +0545

    backup

--- futsal_management/settings.py
+++ futsal_management/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'adminHome/static'),
+    os.path.join(BASE_DIR, 'userHome/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/img/'
 MEDIA_ROOT = BASE_DIR / 'static/img'
